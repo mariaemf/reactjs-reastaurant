1e3412a364ff2c19bc9b2a1554e30986480c1150
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-19 18:52:37 -0300

    I styled the content of the NavBar page using JSX and CSS, in order to make the navbar look good. I used comments only to describe the code and make it easier to understand

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import images from "../../constants/images";
+import { MdOutilineRestaurantMenu } from "react-icons/md";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p_opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p_opensans">
+          <a href="#about">About</a>
+        </li>
+        <li className="p_opensans">
+          <a href="#menu">Menu</a>
+        </li>
+        <li className="p_opensans">
+          <a href="#awards">Awards</a>
+        </li>
+        <li className="p_opensans">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </>
 );
 
 export default Navbar;

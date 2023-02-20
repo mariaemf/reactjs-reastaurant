07bf5c4583098070fdd5da0387f2f83b60fe8192
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-20 20:18:41 -0300

    Work was done to introduce new code with the aim of improving the web application. This included implementing new CSS features and adding content to JSX

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,6 +28,18 @@ const Navbar = () => (
           <a href="#contact">Contact</a>
         </li>
       </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Log In / Registration
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+      </div>
     </nav>
   </>
 );

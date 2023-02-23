492db58222d7336db6140adaed7dddf5f10216b7
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-23 20:26:52 -0300

    I am performing a complete implementation of the header, including improved stylizations and new elements, such as images, buttons, and text. All this to improve the look and functionality of the header and provide a better user experience

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,20 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining </h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        A key to a great dinner is to use fresh.
+        <br /> quality ingredients,prepare them with care, <br />
+        and enjoy good company
+      </p>
+      <button type="button" className="custom__button">
+        {" "}
+        Explore Menu
+      </button>
     </div>
 
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="" />
+    </div>
   </div>
 );
 

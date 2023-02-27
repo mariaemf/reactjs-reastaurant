ce6abd45d24204bb6b4c7879019519d98a9c742b
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-27 14:29:40 -0300

    An increment of new content was performed for the SpecialMenu section, including images, quick styles in the page's JSX, and an additional button for viewing the menu.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import './SpecialMenu.css';
+import { SubHeading, MenuItem } from "../../components";
+import { images, data } from "../../constants";
+
+import "./SpecialMenu.css";
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className=",app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu That You Palatte" />
+      <h1 className="headtext__cormorant">Today`s Special</h1>
+    </div>
+
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine  flex__center">
+        <p className="app__specialMeni_menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.wines.map((wine, index) => (
+            <p>{wine.title}</p>
+          ))}
+        </div>
+      </div>
+
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu img" />
+      </div>
+      <div className="app__specialMenu-menu_cocktails  flex__center">
+        <p className="app__specialMeni_menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktails, index) => (
+            <p>{cocktails.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+
+    <div style={{ marginTop: "15px" }}>
+      <button type="button" className="custom__button">
+        View Menu
+      </button>
+    </div>
   </div>
 );
 

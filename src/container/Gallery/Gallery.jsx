98f76441543ca7e88a5c6f226a261f59a0bced69
mariaemf <mariaeduardamartinsf@gmail.com>
2023-03-05 17:28:43 -0300

    initializing content incrementation in the Gallery session, in order to increment imports and classnames

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
 
-import './Gallery.css';
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+import "./Gallery.css";
+
+const Gallery = () => {
+  return (
+    <>
+      <div className="app__gallery flex__center">
+        <div className="app__gallery-content">
+          <SubHeading title="Instagram" />
+        </div>
+      </div>
+      ;
+    </>
+  );
+};
 
 export default Gallery;

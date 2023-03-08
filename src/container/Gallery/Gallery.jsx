e1ddb1e0331035134639b80b6c979f3ec4e16797
mariaemf <mariaeduardamartinsf@gmail.com>
2023-03-07 21:33:34 -0300

    new increments in the gallery section, comments made to it to make it easier to understand, stylization in the gallery section to leave a more clean and elegant design

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -10,9 +10,23 @@ import { images, data } from "../../constants";
 
 import "./Gallery.css";
 
+const galleryImages = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+];
+
 const Gallery = () => {
   const scrollRef = useRef(null);
 
+  /* 
+  implementação de função de rolagem, onde a mesma e responsavel 
+  por manipular a posicao de rolagem horizontal => se direction 
+  for left, a funcao diminui a posicao de rolagem em 300px 
+  (movendo o scroll para a esquerda)
+  permitindo a navegação pela direita ou esuqerda dentro da galeria de imagens
+  */
   const scroll = (direction) => {
     const { current } = scrollRef;
 
@@ -40,8 +54,28 @@ const Gallery = () => {
             View More
           </button>
         </div>
+        {/*esse codigo possui uma referencia ao scrollref que
+         foi usada para manipular a posicao de rolagem
+        da galeria de imagens, o codigo acaba renderizando uma 
+        lista de imagens dentro do elemento de div, a lista e criada
+        usando o map do array de galleryImagfes que acaba getrando um novo
+        array com elemnto para cada imagem na galeria
+        o componente img foi usado para exibir as imagens na tela com 
+        a propriedade src -> alem disso o codigo define uma key unica para 
+        cada elemento da lista 
+         */}
         <div className="app__gallery-images">
-          <div className="app__gallery-images_container" ref={scrollRef}></div>
+          <div className="app__gallery-images_container" ref={scrollRef}>
+            {galleryImages.map((image, index) => (
+              <div
+                className="app__gallery-images_card flex__center"
+                key={`gallery_image-${index + 1}`}
+              >
+                <img src={image} alt="gallery image" />
+                <BsInstagram className="gallery__image-icon" />
+              </div>
+            ))}
+          </div>
         </div>
         <div className="app__gallery-images_arrow">
           <BsArrowLeftShort

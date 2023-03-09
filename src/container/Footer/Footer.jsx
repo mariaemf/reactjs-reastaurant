3d76688407b41ae49cf89c51a8548fd2ebe27896
mariaemf <mariaeduardamartinsf@gmail.com>
2023-03-09 18:31:28 -0300

    incrementing the footer section and adding some styles to it

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
 import "./Footer.css";
 
-const Footer = () => <div>Footer</div>;
+const Footer = () => (
+  <>
+    <div className="app__footer section__pading">Footer</div>
+    <FooterOverlay />
+    <Newsletter />
+  </>
+);
 
 export default Footer;

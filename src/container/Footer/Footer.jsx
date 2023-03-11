7393778bc10beb7314220ec2e1fc3db681d6a375
mariaemf <mariaeduardamartinsf@gmail.com>
2023-03-10 21:04:38 -0300

    session footer increments

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 import { FooterOverlay, Newsletter } from "../../components";
+import { images } from "../../constants";
 import "./Footer.css";
 
 const Footer = () => (
   <>
-    <div className="app__footer section__pading">Footer</div>
-    <FooterOverlay />
-    <Newsletter />
+    <div className="app__footer section__pading">
+      <FooterOverlay />
+      <Newsletter />
+
+      <div className="app__footer-links">
+        <div className="app__footer-links_contact">
+          <h1 className="app__footer-headtext">Contact Us</h1>
+          <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+          <p className="p__opensans">+1 212-555-1230</p>
+          <p className="p__opensans">+1 212-344-1230</p>
+        </div>
+        <div className="app__footer-links_logo">
+          <img src={images.gericht} alt="footer logo" />
+          <p className="p__opensans">
+            "The best way to find yourself is to lose yourself in the service of
+            others.”
+            <img
+              src={images.spoon}
+              alt="spoon"
+              className="spoon__images"
+              style={{ martginTop: 15 }}
+            />
+          </p>
+        </div>
+
+        <div className="app__footer-links_work"></div>
+      </div>
+      <div className="footer__copyright">
+        <p className="p__opensans">2021 Gerícht. All Rights reserved.</p>
+      </div>
+    </div>
   </>
 );
 
